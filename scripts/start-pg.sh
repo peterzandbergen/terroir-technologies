328b82d31a2d2b25d47ec5e3f64bcb649fe7b9cc
Peter Zandbergen <peter.zandbergen@myhops.com>
2025-05-01 13:53:22 +0200

    Add owner to terroir-portal component

--- scripts/start-pg.sh
+++ scripts/start-pg.sh
@@ -1,9 +1,30 @@
 #!/bin/bash
 
-podman run --detach \
-	--name bspg \
-	-e POSTGRES_PASSWORD=secret \
-	-e PGDATA=/var/lib/postgresql/data/pgdata \
-	-v pgdata:/var/lib/postgresql/data \
-	-p 5432:5432 \
-	docker.io/postgres:alpine
+WDIR=$(dirname $(readlink -f ${BASH_SOURCE[0]}))
+
+source $WDIR/pg-environment
+
+function get-state() {
+	podman inspect --type container "$1" | yq -p json '.[0].State.Status'
+}
+
+function run-container() {
+	podman run --detach \
+		--name "$1" \
+		-e POSTGRES_PASSWORD=secret \
+		-e PGDATA=/var/lib/postgresql/data/pgdata \
+		-v pgdata:/var/lib/postgresql/data \
+		-p 5432:5432 \
+		docker.io/postgres:alpine 2>/dev/null
+}
+
+function start-container() {
+	podman start "$1"
+}
+
+CONTAINER_NAME=bspg
+
+run-container $CONTAINER_NAME
+start-container $CONTAINER_NAME
+
+echo Container $CONTAINER_NAME has state $(get-state $CONTAINER_NAME)
